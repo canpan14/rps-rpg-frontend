d9dd0d9d94aa0e51173e80ae15af24a10ee8255a
canpan14 <sammachlin@hotmail.com>
2017-10-23 09:51:15 -0400

    Add state class

---
+++ assets/scripts/rpsrpg/state.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const State = function (state) {
+  this.advHealth = parseInt(state.a_health)
+  this.advAttack = parseInt(state.a_attack)
+  this.inFight = state.in_fight
+  this.enemyName = state.e_name
+  this.enemyLevel = parseInt(state.e_level)
+  this.enemyExp = parseInt(state.e_exp)
+  this.enemyAttack = parseInt(state.e_attack)
+  this.enemyHealth = parseInt(state.e_health)
+  this.enemyRockChance = parseFloat(state.e_rock_chance)
+  this.enemyPaperChance = parseFloat(state.e_paper_chance)
+  this.enemyScissorChance = parseFloat(state.e_scissor_chance)
+  this.enemyLearningCurve = parseFloat(state.e_learning_curve)
+}
+
+const createState = function (state) {
+  return new State(state)
+}
+
+module.exports = {
+  createState
+}
